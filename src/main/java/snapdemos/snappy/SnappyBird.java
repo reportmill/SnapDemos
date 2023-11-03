03eafc64ca6057dcb398e399e7ea296f43806b5f
Jeff Martin <jeff@reportmill.com>
2023-11-03 16:35:37 -0500

    SnappyBird: Some cleanup

--- src/main/java/snapdemos/snappy/SnappyBird.java
+++ src/main/java/snapdemos/snappy/SnappyBird.java
@@ -2,11 +2,8 @@ package snapdemos.snappy;
 import java.util.Random;
 import snap.geom.*;
 import snap.gfx.*;
-import snap.text.TextDoc;
 import snap.view.*;
-import snap.viewx.CodeDoc;
 import snap.viewx.Explode;
-import snap.web.WebURL;
 
 /**
  * A Flappy-Bird app.
@@ -14,23 +11,34 @@ import snap.web.WebURL;
 public class SnappyBird extends ViewOwner {
 
     // The main view
-    ChildView _view;
+    private ChildView _mainView;
 
     // The flappy view
-    ImageView _flappy;
+    private ImageView _flappyView;
 
-    // The velocity of flappy
-    double _veloc;
+    // The view that holds pipes (top/bottom base + caps)
+    private ColView _pipesView;
 
-    // The elapsed time
-    int _time;
+    // The time at last frame
+    private long _lastFrameTime;
 
-    // The acceleration of gravity (points/sec^2)
-    double GRAVITY = 75;
+    // The current vertical speed of flappy (points / sec)
+    private double _flappySpeedY;
+
+    // The acceleration of gravity (points / sec^2)
+    private double GRAVITY = 75;
 
     // The image for the pipe
     Image FLAPPY_IMAGE = Image.getImageForClassResource(getClass(), "Snappy.png");
     Image PIPE_IMAGE = Image.getImageForClassResource(getClass(), "SnappyPipe.png");
+
+    /**
+     * Constructor.
+     */
+    public SnappyBird()
+    {
+        super();
+    }
 
     /**
      * Create UI.
@@ -38,145 +46,147 @@ public class SnappyBird extends ViewOwner {
     protected View createUI()
     {
         // Create main view
-        _view = new ChildView();
-        _view.setPrefSize(800, 600);
-        _view.setFill(new Color("#EEFFFF"));
-        _view.setClipToBounds(true);
-        _view.setBorder(Color.GRAY, 1);
-        _view.addEventHandler(e -> flap(), MouseRelease);
+        _mainView = new ChildView();
+        _mainView.setPrefSize(800, 600);
+        _mainView.setFill(new Color("#EEFFFF"));
+        _mainView.setClipToBounds(true);
+        _mainView.setBorder(Color.GRAY, 1);
+        _mainView.addEventHandler(e -> flap(), MousePress);
 
         // Create Flappy Image and ImageView and add
-        _flappy = new ImageView(FLAPPY_IMAGE);
-        _flappy.setBounds(180, 180, 60, 45);
-        _view.addChild(_flappy);
-
-        // Create CodeView
-        TextArea codeView = new TextArea();
-        codeView.setFill(Color.WHITE);
-        codeView.setEditable(true);
-
-        // Create CodeDoc and set in TextView
-        WebURL javaURL = WebURL.getURL(getClass(), "SnappyBird.jav");
-        TextDoc javaTextDoc = CodeDoc.newFromSource(javaURL);
-        codeView.setSourceText(javaTextDoc);
-
-        // Create TabView
-        TabView tabView = new TabView();
-        tabView.setGrowWidth(true);
-        tabView.setBorder(Color.GRAY, 1);
-        tabView.setGrowWidth(true);
-
-        // Create/add ScaleBox
-        BoxView scaleBox = new ScaleBox(_view, true, true);
-        scaleBox.setPadding(10, 10, 10, 10);
-        tabView.addTab("SnappyBird", scaleBox);
-
-        // Create/add CodeView
-        tabView.addTab("Source", new ScrollView(codeView));
+        _flappyView = new ImageView(FLAPPY_IMAGE);
+        _flappyView.setBounds(180, 220, 60, 45);
+        _mainView.addChild(_flappyView);
 
         // Return
-        return tabView;
+        return _mainView;
     }
 
     /**
-     * Initialize UI.
+     * Called when MainView is showing.
      */
-    protected void initUI()
+    @Override
+    protected void initShowing()
     {
-        addPipe();
-        new ViewTimer(40, t -> updateTime(t)).start(500);
-        getWindow().setGrowWidth(true);
+        // Create the pipes view and add to MainView
+        _pipesView = createPipesView();
+        _mainView.addChild(_pipesView);
+
+        // Reset pipe heights and animation
+        resetPipesViewGapAndAnimation();
+
+        // Get start time and start calling update frame method
+        _lastFrameTime = System.currentTimeMillis();
+        getEnv().runIntervals(this::updateFrame, 25);
     }
 
-    void addPipe()
+    /**
+     * Creates the view that holds top/bottom pipes.
+     */
+    private ColView createPipesView()
     {
         // Create 4 pipe parts (top base+cap and bottom base+cap)
-        View p1 = createPipePart();
-        p1.setPrefWidth(64);
-        View p2 = createPipePart();
-        p2.setPrefSize(90, 40);
-        View p3 = createPipePart();
-        p3.setPrefSize(90, 40);
-        p3.setLeanY(VPos.BOTTOM);
-        View p4 = createPipePart();
-        p4.setPrefWidth(64);
+        View topPipeBase = createPipePart();
+        topPipeBase.setPrefWidth(64);
+        View topPipeCap = createPipePart();
+        topPipeCap.setPrefSize(90, 40);
 
-        // Create Pipe VBox view to hold pipe parts
-        ColView vbox = new ColView();
-        vbox.setAlign(Pos.TOP_CENTER);
-        vbox.setPrefHeight(_view.getPrefHeight());
-        vbox.setChildren(p1, p2, p3, p4);
-        vbox.setSize(vbox.getBestSize());
-        _view.addChild(vbox);
-        resetPipe(vbox);
+        // Create bottom pipe parts (cap and base)
+        View bottomPipeCap = createPipePart();
+        bottomPipeCap.setPrefSize(90, 40);
+        bottomPipeCap.setLeanY(VPos.BOTTOM);
+        View bottomPipeBase = createPipePart();
+        bottomPipeBase.setPrefWidth(64);
+
+        // Create col view to hold pipe parts
+        ColView pipesView = new ColView();
+        pipesView.setAlign(Pos.TOP_CENTER);
+        pipesView.setPrefHeight(_mainView.getPrefHeight());
+        pipesView.setChildren(topPipeBase, topPipeCap, bottomPipeCap, bottomPipeBase);
+        pipesView.setSize(pipesView.getBestSize());
+
+        // Return
+        return pipesView;
     }
 
-    View createPipePart()
+    /**
+     * Creates a pipe part.
+     */
+    private View createPipePart()
     {
-        ImageView pipe = new ImageView(PIPE_IMAGE, true, true);
-        pipe.setBorder(new Color("#008000"), 2);
-        return pipe;
+        ImageView imageView = new ImageView(PIPE_IMAGE, true, true);
+        imageView.setBorder(new Color("#008000"), 2);
+        return imageView;
     }
 
-    void resetPipe(ColView aPipe)
+    /**
+     * Resets the pipes view pipe gap and animation.
+     */
+    private void resetPipesViewGapAndAnimation()
     {
-        // Reset pipe levels
-        double dh = new Random().nextInt(3) * 100 - 100;
-        aPipe.getChild(0).setPrefHeight(200 + dh);
-        aPipe.getChild(3).setPrefHeight(200 - dh);
+        // Reset top/bottom pipe base heights by random offset
+        double heightOffset = new Random().nextInt(3) * 100 - 100;
+        _pipesView.getChild(0).setPrefHeight(200 + heightOffset);
+        _pipesView.getChild(3).setPrefHeight(200 - heightOffset);
 
         // Reset pipe location
-        aPipe.setX(_view.getPrefWidth());
-        ViewAnim anim = aPipe.getAnim(4000);
-        anim.setX(-aPipe.getWidth());
-        anim.setOnFinish(() -> getEnv().runLater(() -> resetPipe(aPipe)));
+        double backgroundW = _mainView.getPrefWidth();
+        _pipesView.setX(backgroundW);
+
+        // Reset pipe animation (from right side of screen to off-screen left)
+        ViewAnim anim = _pipesView.getAnimCleared(4000);
+        anim.setX(-_pipesView.getWidth());
+        anim.setOnFinish(() -> runLater(() -> resetPipesViewGapAndAnimation()));
         anim.play();
     }
 
     /**
-     * Called on MouseRelease on main view.
+     * Called on MousePress on main view.
      */
-    void flap()
+    private void flap()
     {
-        _veloc -= 60;
-        if (_veloc < -80) _veloc = -80;
+        _flappySpeedY -= 60;
+        if (_flappySpeedY < -80)
+            _flappySpeedY = -80;
     }
 
     /**
-     * Called when timer updates.
+     * Called repeatedly to update frame.
      */
-    void updateTime(ViewTimer aTimer)
+    private void updateFrame()
     {
-        // Get time change since last update
-        int time = aTimer.getTime();
-        double dt = (time - _time) / 1000d;
-        _time = time;
+        // Get time change since last frame update
+        long time = System.currentTimeMillis();
+        double dt = (time - _lastFrameTime) / 1000d;
+        _lastFrameTime = time;
 
         // If flappy exploding, just return
-        if (_flappy.getOpacity() != 1)
+        if (_flappyView.getOpacity() != 1)
             return;
 
-        // Calculate change in y and update: dist = accel*time^2 + veloc*time
-        double dy = _veloc * dt;
-        double ny = _flappy.getY() + dy;
-        if (ny > _view.getHeight() - 100) ny = _view.getHeight() - 100;
-        _flappy.setY(ny);
+        // Calculate change in flappy y and update: distY = accelY * time ^ 2 + speedY * time
+        double dy = _flappySpeedY * dt;
+        double newY = _flappyView.getY() + dy;
+        if (newY > _mainView.getHeight() - 100)
+            newY = _mainView.getHeight() - 100;
+        _flappyView.setY(newY);
 
         // Update velocity
-        _veloc += GRAVITY * dt;
-        if (_veloc > 80) _veloc = 80;
+        _flappySpeedY += GRAVITY * dt;
+        if (_flappySpeedY > 80)
+            _flappySpeedY = 80;
 
         // Update rotation
-        _flappy.setRotate(_veloc / 80 * 30);
+        _flappyView.setRotate(_flappySpeedY / 80 * 30);
 
         // If flappy hit pipe, explode and reset attributes
-        ViewList vlist = _flappy.getParent().getViewList();
-        View hitView = vlist.getHitView(_flappy, null, 8);
-        if (hitView != null) {
-            new Explode(_flappy, 10, 10, () -> explodeDone()).play();
-            _veloc = 0;
-            _flappy.setRotate(0);
-            _flappy.setY(250);
+        ViewList viewList = _mainView.getViewList();
+        View viewHitByFlappyView = viewList.getHitView(_flappyView, null, 8);
+        if (viewHitByFlappyView != null) {
+            new Explode(_flappyView, 20, 20, () -> explodeDone()).play();
+            _flappySpeedY = 0;
+            _flappyView.setRotate(0);
+            _flappyView.setY(250);
         }
     }
 
@@ -185,16 +195,15 @@ public class SnappyBird extends ViewOwner {
      */
     private void explodeDone()
     {
-        _flappy.getAnimCleared(800).setOpacity(1).play();
+        _flappyView.getAnimCleared(800).setOpacity(1).play();
     }
 
     /**
      * Standard main method.
      */
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         SnappyBird snappyBird = new SnappyBird();
-        snappyBird.getUI().setGrowWidth(true);
         snappyBird.setWindowVisible(true);
     }
 }
